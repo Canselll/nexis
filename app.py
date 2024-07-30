d2b231ee150f5de5282c3656be4e5b82ff325844
Canselll <canselkandemir32@gmail.com>
2024-07-30 16:15:01 +0000

    regulations

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from app.service import Serve
+from storage.file_storage import SaveFile
 
 serve = Serve()
 result = serve.serve()
 
-print(result)
+save_file = SaveFile()
+save_file.save_result_to_file(result,'StatusReport.txt')
+
+
--- storage/file_storage.py
+++ storage/file_storage.py
@@ -1,20 +1,14 @@
 import os
-from app import result  
 
 class SaveFile():
     
-    def save_result_to_file(file_name):
+    def save_result_to_file(self ,result, file_name):
         
-        file_path = os.path.join('storage/StatusReport.txt', file_name)
+        file_path = os.path.join("storage", file_name)
+
         
         with open(file_path, 'w') as file:
             file.write(result)
         
         print(f"Result saved to {file_path}")
-
-
-save_file = SaveFile()
-save_file.save_result_to_file('result.txt')
-
-
 

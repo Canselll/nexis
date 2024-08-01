1fe17351c62722db35bc5719d41b000e74248bb1
Canselll <canselkandemir32@gmail.com>
2024-08-01 09:45:24 +0000

    regulation

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from app.service import Serve
-from storage.file_storage import SaveFile
+from app.service import Serve 
 
 serve = Serve()
 result = serve.serve()
 
-save_file = SaveFile()
-save_file.save_result_to_file(result,'StatusReport.txt')
+
 
 
--- app/service.py
+++ app/service.py
@@ -1,11 +1,14 @@
 from .preservice.conclude import Apply
+from storage.file_storage import SaveFile
 
-class Serve(Apply):
+class Serve(Apply,SaveFile):
 
     def serve(self):
 
         df = self.apply_data_processing()
         result = self.apply_crew_kick_off(df)
+        save_file = self.save_result_to_file(result,'StatusReport.txt') 
 
-        return result
+        return save_file
+    
     
